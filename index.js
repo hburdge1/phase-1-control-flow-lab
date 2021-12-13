617f2ad716812ec4b6da5d86f9ce859cb6ef879c
Hannah Burdge <hburdge1@gmail.com>
2021-12-13 11:05:48 -0800

    commit tree 6fba955bdb4f3b8c5c13c8a786d7a2fdba6c3e7a

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(someThing) {
+  let responseToFeet 
+  if (someThing <= 400) {
+    responseToFeet = 'This one is on me!';
+  } else if ((someThing > 2000) && (someThing <= 2500)) {
+    responseToFeet = 'I will gladly take your thirty bucks.'
+  } else if (someThing > 2500) {
+    responseToFeet = 'No can do.'
+  }
+  return(responseToFeet)
+};
+  
+function ternaryCheckCity(destination) {
+  let tellThem
+  destination === 'NYC' ? (tellThem = "Ok, sounds good.") : (tellThem = 'No go.' )
+  return tellThem;
+};
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(cashMoney) {
+  let Charm ;
+  switch (cashMoney) {
+    case 'generous' :
+    Charm = 'Thank you so much.'
+    break ;
+    case 'not as generous' :
+    Charm = 'Thank you.'
+    break ;
+    default:
+    Charm = 'Bye.'
+  }
+  return Charm ;
 }
